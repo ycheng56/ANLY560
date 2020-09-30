e032ecd3e88ef3c3bfd998da66f734f652074488
Cynthia Cheng <32085874+ycheng56@users.noreply.github.com>
2020-09-29 21:00:39 -0400

    Add files via upload

---
+++ week 5/Assignment2.java
@@ -0,0 +1,45 @@
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+
+		double grade1= 91.14; 
+		double grade2= 85.01;
+		double grade3= 72;
+		double grade4= 63;
+		double grade5= 81.14;
+		double grade_avg;
+		grade_avg = (grade1 + grade2 + grade3 + grade4 + grade5)/5;
+		
+		char G;
+		if (grade_avg>=90.00 && grade_avg<100.00) {
+			G = 'A';
+		}
+		else if(grade_avg>=80.00 && grade_avg<90.00){
+			G = 'B';
+		}
+		else if(grade_avg>=70.00 && grade_avg<80.00){
+			G = 'C';
+		}
+		else {
+			G = 'F';
+		}
+		
+
+		switch (G) {
+			case 'A':
+				System.out.println("A: Fantastic job!");break;
+			case 'B':
+				System.out.println("B: Good!");break;		
+			case 'C':
+				System.out.println("C: Please try more next time!");break;
+			case 'F':
+				System.out.println("F: Unfortunately you failed!");break;
+	        default :
+	        	System.out.println("Invalid grade");
+		}
+
+
+	}
+
+}
